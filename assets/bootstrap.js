430a8354692dfb83319c48510296f7e6ca23f3aa
Cameron Cardinal <cameriancardinal@gmail.com>
2024-06-12 22:08:21 +0200

    changed navbar so it's lighter

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,5 @@
-import { startStimulusApp } from '@symfony/stimulus-bridge';
+// import { startStimulusApp } from '@symfony/stimulus-bridge';
 
-const app = startStimulusApp();
-// register any custom, 3rd party controllers here
-// app.register('some_controller_name', SomeImportedController);
+// const app = startStimulusApp();
+// // register any custom, 3rd party controllers here
+// // app.register('some_controller_name', SomeImportedController);

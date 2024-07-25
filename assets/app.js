693e079223a1ca99174d2f2bce37da0e2c9096b5
Cameron Cardinal <cameriancardinal@gmail.com>
2024-07-26 00:56:58 +0200

    added content for homepage + added link to downloadable resume in skills

--- assets/app.js
+++ assets/app.js
@@ -7,12 +7,14 @@ import './bootstrap';
  */
 import './styles/app.css';
 
+// toggle nav with hamburger in mobile view
 let nav = document.querySelector("nav");
 let hamburger = document.querySelector("#hamburger");
 hamburger.addEventListener("click", () => {
     nav.classList.toggle("open");
 })
 
+// automatically remove class open when links are clickled
 let links = document.querySelectorAll(".nav-links");
 links.forEach((liens) => {
     liens.addEventListener("click", () => {
@@ -20,26 +22,25 @@ links.forEach((liens) => {
     })
 })
 
+// detect user preferences for light/dark mode
 const runColorMode = (fn) => {
     if (!window.matchMedia) {
       return;
     }
-    
     const query = window.matchMedia('(prefers-color-scheme: dark)');
-  
-    fn(query.matches);
-  
+    fn(query.matches);  
     query.addEventListener('change', (event) => fn(event.matches));
-  }
+}
   
-  runColorMode((isDarkMode) => {
-    if (isDarkMode) {
-        document.body.classList.add('dark-mode');
-    } else {
-        document.body.classList.add('dark-mode');
-    }
-  })
+runColorMode((isDarkMode) => {
+   if (isDarkMode) {
+    document.body.classList.add('dark-mode');
+   } else {
+    document.body.classList.add('dark-mode');
+   }
+})
 
+// button toggle light/dark mode 
 let darkModeButton = document.querySelector("#dark-mode");
 darkModeButton.addEventListener("click", () => {
     document.body.classList.toggle('dark-mode');

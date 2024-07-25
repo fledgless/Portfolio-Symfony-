f8bbf2aae75f72021d5d5f474d1e2ba57c326143
Cameron Cardinal <cameriancardinal@gmail.com>
2024-07-25 11:56:58 +0200

    added function to detect light/dark mode preferences

--- assets/app.js
+++ assets/app.js
@@ -19,3 +19,23 @@ links.forEach((liens) => {
         nav.classList.remove("open");
     })
 })
+
+const runColorMode = (fn) => {
+    if (!window.matchMedia) {
+      return;
+    }
+    
+    const query = window.matchMedia('(prefers-color-scheme: dark)');
+  
+    fn(query.matches);
+  
+    query.addEventListener('change', (event) => fn(event.matches));
+  }
+  
+  runColorMode((isDarkMode) => {
+    if (isDarkMode) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.add('dark-mode');
+    }
+  })

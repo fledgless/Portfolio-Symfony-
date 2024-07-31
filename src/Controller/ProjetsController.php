6865841af026f92fc36088c26290e49250a9bb94
Cameron Cardinal <cameriancardinal@gmail.com>
2024-07-31 04:30:43 +0200

    added list for projects in Projets, need to add style and tags

--- src/Controller/ProjetsController.php
+++ src/Controller/ProjetsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Attribute\Route;
 class ProjetsController extends AbstractController
 {
     #[Route('/projets', name: 'app_projets')]
-    public function index(): Response
+    public function index(ProjectRepository $projectRepo): Response
     {
         return $this->render('projets/index.html.twig', [
-            'controller_name' => 'ProjetsController',
+            'projects' => $projectRepo->findAll(),
         ]);
     }
 }
